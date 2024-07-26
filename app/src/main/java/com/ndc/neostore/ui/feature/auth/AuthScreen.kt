ca40b9ae0bd2a1cd0374ac12c9558a18d22c9e23
fauzanramadhani <fauzanramadhani06@gmail.com>
2024-07-27 04:52:46 +0700

    adjustment: remove unused import

--- app/src/main/java/com/ndc/neostore/ui/feature/auth/AuthScreen.kt
+++ app/src/main/java/com/ndc/neostore/ui/feature/auth/AuthScreen.kt
@@ -1,7 +1,6 @@
 package com.ndc.neostore.ui.feature.auth
 
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import com.ndc.neostore.ui.feature.auth.screen.LoginScreen
