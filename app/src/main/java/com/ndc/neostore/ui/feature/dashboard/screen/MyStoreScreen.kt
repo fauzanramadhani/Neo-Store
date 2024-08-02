5f69dc2a3c076e63b2cf3dd511396321821b4e7b
fauzanramadhani <fauzanramadhani06@gmail.com>
2024-08-02 15:08:56 +0700

    feat: add empty state

--- app/src/main/java/com/ndc/neostore/ui/feature/dashboard/screen/MyStoreScreen.kt
+++ app/src/main/java/com/ndc/neostore/ui/feature/dashboard/screen/MyStoreScreen.kt
@@ -1,14 +1,24 @@
 package com.ndc.neostore.ui.feature.dashboard.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.ndc.neostore.R
 import com.ndc.neostore.ui.component.card.MyProductCard
 import com.ndc.neostore.ui.feature.dashboard.DashboardAction
 import com.ndc.neostore.ui.feature.dashboard.DashboardState
@@ -35,20 +45,44 @@ fun MyStoreScreen(
         ),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        items(
-            items = state.myProductList,
-            key = {
-                it.productId
+        when {
+            state.myProductList.isEmpty() -> item {
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.empty_illustration),
+                        contentDescription = ""
+                    )
+                    Text(
+                        text = "Ups... Sepertinya Anda belum memiliki produk",
+                        style = typography.bodyMedium,
+                        color = color.onBackground,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
+                }
             }
-        ) {
-            MyProductCard(
-                productName = it.productName,
-                productImage = it.productImageUrl,
-                stock = it.productStock,
-                price = it.productPrice
+            else -> items(
+                items = state.myProductList,
+                key = {
+                    it.productId
+                }
             ) {
-                navHostController.navigate(NavRoute.EditProductScreen.navigateWithId(it.productId)) {
-                    launchSingleTop = true
+                MyProductCard(
+                    productName = it.productName,
+                    productImage = it.productImageUrl,
+                    stock = it.productStock,
+                    price = it.productPrice
+                ) {
+                    navHostController.navigate(NavRoute.EditProductScreen.navigateWithId(it.productId)) {
+                        launchSingleTop = true
+                    }
                 }
             }
         }
--- app/src/main/java/com/ndc/neostore/ui/feature/transaction/screen/SalesScreen.kt
+++ app/src/main/java/com/ndc/neostore/ui/feature/transaction/screen/SalesScreen.kt
@@ -1,20 +1,29 @@
 package com.ndc.neostore.ui.feature.transaction.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.ndc.neostore.R
 import com.ndc.neostore.ui.component.card.TransactionCard
 import com.ndc.neostore.ui.component.shimmer.shimmerBrush
 import com.ndc.neostore.ui.feature.transaction.TransactionAction
@@ -28,6 +37,9 @@ fun SalesScreen(
     state: TransactionState,
     action: (TransactionAction) -> Unit,
 ) {
+    val color = MaterialTheme.colorScheme
+    val typography = MaterialTheme.typography
+
     LazyColumn(
         state = listState,
         verticalArrangement = Arrangement.spacedBy(16.dp),
@@ -42,6 +54,29 @@ fun SalesScreen(
                         .clip(RoundedCornerShape(8.dp))
                         .background(shimmerBrush())
                 )
+            }
+
+            state.mySalesOrderList.isEmpty() -> item {
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.empty_illustration),
+                        contentDescription = ""
+                    )
+                    Text(
+                        text = "Ups... Sepertinya Anda belum memiliki produk yang terjual",
+                        style = typography.bodyMedium,
+                        color = color.onBackground,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
+                }
             }
 
             else -> items(
